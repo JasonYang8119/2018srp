e7b7405ee6f17a5817eebf7292be3e57fe12c85a
JasonYang8119 <JasonYang8119@users.noreply.github.com>
2018-09-13 10:32:11 +0800

    修改参数，注意论文中使用d/lumbda=1/2，满足论文结果

--- myCode/SD_CPA.m
+++ myCode/SD_CPA.m
@@ -3,17 +3,19 @@ close all;
 clc;
 %设置实际阵列
 M = 4; N=5;
-S=[(0:N-1)*M,(1:M-1)*N].';  
+S=[(0:N-1)*M,(1:M-1)*N].';
 LEN_S = length(S);
 
 %生成源信号
-theta_bar = linspace(-60,60,5);  %实际DOA角度
+theta_bar = linspace(-60,60,25);  %实际DOA角度
 DD = length(theta_bar);
 SNRdB = 0;
 SNAPSHOTS = 2000;
-A_S = exp(2i * pi * S * sind(theta_bar));
-
-Source = randn(DD, SNAPSHOTS);
+d_lumbda = 1/2;
+A_S = exp(2i * pi * d_lumbda * S * sind(theta_bar));
+for jj = 1:DD
+Source(jj,:) = sin(2 * pi * randn(1, SNAPSHOTS));
+end
 Noise = (randn(LEN_S, SNAPSHOTS) + 1i * randn(LEN_S, SNAPSHOTS)) / sqrt(2);
 noise_std = 10^(-SNRdB/20);
 
@@ -34,8 +36,8 @@ DOF = 29;  %子阵个数
 m = length(Z_vec) - DOF + 1;  %每个子阵阵元数
 R_zz = zeros(m,m);
 for j = 1:DOF
-   Z_i  = Z_vec(j:j+m-1);
-   R_zz = R_zz + Z_i*Z_i';
+    Z_i  = Z_vec(j:j+m-1);
+    R_zz = R_zz + Z_i*Z_i';
 end
 R_zz = R_zz/DOF;
 
@@ -44,10 +46,10 @@ R_zz = R_zz/DOF;
 Un=U(:,DD+1:m);
 
 theta=-60:0.1:60;
- for ii=1:length(theta)
-   a=exp(2i * pi * (0:m-1)' * sind(theta(ii)));
-   Psmoothmusic(ii)=1./abs(a'*Un*Un'*a);
- end
+for ii=1:length(theta)
+    a=exp(2i * pi *  d_lumbda * (0:m-1)' * sind(theta(ii)));
+    Psmoothmusic(ii)=1./abs(a'*Un*Un'*a);
+end
 
 plot(theta,10*log(Psmoothmusic)','-r');
 xlabel('入射角/度');
